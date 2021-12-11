e60de1ecb0a1fcd54f6ea1c8041a07355c7f0b5a
OperasScripts <95578166+OperasScripts@users.noreply.github.com>
2021-12-11 09:45:07 +0000

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,13 @@
+--[[
+   Developed by:
+    Opera#1759
+    Marco X#4505
+    
+    Opera's Hub - loader
+]]
+
+if game.PlaceId == 7816280053 then
+	loadstring(game:HttpGet("https://pastebin.com/raw/ZsdGu6G9"))()
+--elseif game.PlaceId == 1962086868 then
+	--loadstring(game:HttpGet("url"))()
+end
